20fe09234652cec78664bafabd6b6fb9b070c12c
Janardhan9392 <112267368+Janardhan9392@users.noreply.github.com>
2022-09-27 20:11:47 +0530

    Add files via upload

---
+++ pascal sum traingular.py
@@ -0,0 +1,13 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\pedab\AppData\Local\Programs\Python\Python310\find max,min in list.py
+Enter a number of rows:6
+Enter the row number:4
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+Sum of numbers: 8
