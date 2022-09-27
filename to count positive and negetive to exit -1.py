d8b1b10c56f34c73b332371fadcd946eba5091cd
Janardhan9392 <112267368+Janardhan9392@users.noreply.github.com>
2022-09-27 13:47:39 +0530

    Add files via upload

---
+++ to count positive and negetive to exit -1.py
@@ -0,0 +1,9 @@
+number = float(input(" Please Enter any Numeric Value : "))
+if(number > 0):
+    print("{0} is a Positive Number".format(number))
+elif(number < 0):
+    print("{0} is a Negative Number".format(number))
+else:
+    print("You have entered Zero")
+    while(number==-1):
+        print("exit")
