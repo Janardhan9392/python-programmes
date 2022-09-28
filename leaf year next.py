6a3d6fcdc0fa9e85d56257ad6a9e1d045deeb997
Janardhan9392 <112267368+Janardhan9392@users.noreply.github.com>
2022-09-28 14:40:07 +0530

    Add files via upload

---
+++ leaf year next.py
@@ -0,0 +1,19 @@
+try:
+    year=int(input("enter the year"))
+except:
+    ValueError
+    print("enter the proper value")
+else:
+    for i in range(0,5):
+        if(year<=0):
+            print("invalid")
+
+        if(year%4==0 or year%400==0 and year%100!=0):
+            print("it is a leaf year",year)
+    
+        else:
+            print("its not a leaf year",year)
+            year=year+1
+            print("the next leaf year s:",year)
+    
+print()
---
+++ palindrome true or false.py
@@ -0,0 +1,7 @@
+string=input(("Enter a string:"))
+if(string==string[::-1]):
+         print(True)
+else:
+      print(False)
+print()
+ 
